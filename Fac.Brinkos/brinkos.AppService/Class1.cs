3bd4879bf6dd5046d6bc46479fc532041ed4047b
rcastellanos <rcastellanos@1XYBXF2>
2018-10-03 17:14:16 -0600

    Se agrego a la solucion el proyecto de dominio

--- Fac.Brinkos/brinkos.AppService/Class1.cs
+++ Fac.Brinkos/brinkos.AppService/Class1.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace brinkos.AppService
+﻿namespace brinkos.AppService
 {
-    class Class1
+    internal class Class1
     {
     }
 }
